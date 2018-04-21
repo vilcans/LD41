0e56fbc80bdd347a0d4924d227a19e251698c4c9
Martin Vilcans <martin@librador.com>
2018-04-21 18:45:57 +0200

    Generate level procedurally

--- Assets/Level.cs
+++ Assets/Level.cs
@@ -17,14 +17,7 @@ public class Level : MonoBehaviour {
     private Tile[,] tiles;
 
     public void Create() {
-        tiles = new Tile[height, width];
-        for(int row = 0; row < height; ++row) {
-            for(int column = 0; column < width; ++column) {
-                Tile tile = (Tile)UnityEngine.Random.Range(0, 2);
-                tiles[row, column] = tile;
-                //Debug.LogFormat("tile row {0} col {1} = {2}", row, column, tile);
-            }
-        }
+        tiles = GenerateTiles();
 
         for(int row = 0; row < height; ++row) {
             for(int column = 0; column < width; ++column) {
@@ -62,5 +55,52 @@ public class Level : MonoBehaviour {
         SpriteRenderer r = obj.AddComponent<SpriteRenderer>();
         r.sprite = sprite;
         return obj;
+    }
+
+    private static Tile[,] GenerateTiles() {
+        Tile[,] tiles = new Tile[height, width];
+
+        for(int row = 0; row < height; ++row) {
+            for(int column = 0; column < width; ++column) {
+                Tile tile = (Tile)UnityEngine.Random.Range(0, 2);
+                tiles[row, column] = tile;
+                //Debug.LogFormat("tile row {0} col {1} = {2}", row, column, tile);
+            }
+        }
+
+        Tile[,] nextGeneration = new Tile[height, width];
+        for(int generation = 0; generation < 3000; ++generation) {
+            for(int row = 0; row < height; ++row) {
+                for(int column = 0; column < width; ++column) {
+                    Tile tile = tiles[row, column];
+                    int neighbors = CountNeighbors(tiles, new Vector2Int(column, row), tile);
+                    if(tile == Tile.Rock && neighbors < 2) {
+                        nextGeneration[row, column] = Tile.Floor;
+                    }
+                    else if(tile == Tile.Floor && neighbors < 2) {
+                        nextGeneration[row, column] = Tile.Rock;
+                    }
+                    else {
+                        nextGeneration[row, column] = tile;
+                    }
+                }
+            }
+            {
+                var tmp = nextGeneration;
+                nextGeneration = tiles;
+                tiles = tmp;
+            }
+        }
+
+        return tiles;
+    }
+
+    private static int CountNeighbors(Tile[,] tiles, Vector2Int square, Tile soughtTile) {
+        int neighbors = 0;
+        if(square.x >= 1 && tiles[square.y, square.x - 1] == soughtTile) { ++neighbors; }
+        if(square.x < width - 1 && tiles[square.y, square.x + 1] == soughtTile) { ++neighbors; }
+        if(square.y >= 1 && tiles[square.y - 1, square.x] == soughtTile) { ++neighbors; }
+        if(square.y < height - 1 && tiles[square.y + 1, square.x] == soughtTile) { ++neighbors; }
+        return neighbors;
     }
 }
