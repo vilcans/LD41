6d424e4d42f8da06f66cdea55a9af470b8418c4d
Martin Vilcans <martin@librador.com>
2018-04-21 12:58:19 +0200

    Rotate arrow on beats

---
+++ Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public AudioSource music;
+    public Transform rotator;
+
+    private float bpm = 120;
+    private float samplesPerBeat;
+
+    public void Awake() {
+        samplesPerBeat = music.clip.frequency * 60 / bpm;
+    }
+
+    public void Update() {
+        int time = music.timeSamples;
+        float beat = time / samplesPerBeat;
+        beat = Mathf.Floor(beat);
+        float angle = beat * 90;
+
+        rotator.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+    }
+}
