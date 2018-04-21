8350b9af361735c64f4568f0a0dea7922d67eb7f
Martin Vilcans <martin@librador.com>
2018-04-21 14:08:40 +0200

    Moving

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
 
     public AudioSource music;
+    public Transform playerTransform;
+    public Transform visualTransform;
     public Transform rotator;
+
+    private struct Direction {
+        public Vector2Int deltaPosition;
+
+        public static Direction up = new Direction { deltaPosition = new Vector2Int(0, 1) };
+        public static Direction right = new Direction { deltaPosition = new Vector2Int(1, 0) };
+        public static Direction down = new Direction { deltaPosition = new Vector2Int(0, -1) };
+        public static Direction left = new Direction { deltaPosition = new Vector2Int(-1, 0) };
+        public static Direction[] directions = { up, right, down, left };
+    }
 
     private float bpm = 120;
     private float samplesPerBeat;
+
+    private Vector2Int playerPosition;
 
     public void Awake() {
         samplesPerBeat = music.clip.frequency * 60 / bpm;
@@ -17,9 +29,24 @@ public class Game : MonoBehaviour {
     public void Update() {
         int time = music.timeSamples;
         float beat = time / samplesPerBeat;
-        beat = Mathf.Floor(beat);
-        float angle = beat * 90;
 
-        rotator.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+        int index = (int)(beat + .5) % 4;
+
+        Direction direction = Direction.directions[index];
+
+        rotator.rotation = Quaternion.AngleAxis(-(int)(beat) * 90, Vector3.forward);
+
+        if(Input.anyKeyDown) {
+            playerPosition += direction.deltaPosition;
+            float diff = (int)(beat) - beat;
+            Debug.LogFormat("diff = {0}", diff);
+        }
+
+        playerTransform.position = GridToWorldPosition(playerPosition);
+        visualTransform.localScale = Vector3.one * Mathf.Lerp(1.3f, 1.0f, 1.0f - 1.0f / (beat - (int)beat + 1));
+    }
+
+    private Vector3 GridToWorldPosition(Vector2Int p) {
+        return new Vector3(p.x, p.y, 0);
     }
 }
